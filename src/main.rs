ea89bebd913f6b434a0b34de35fa241b985ab696
Jacobo-EG <0619975360@uma.es>
2025-02-26 11:11:07 +0100

    Added the plot function

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::f64::consts::PI;
 extern crate gnuplot;
 use gnuplot::{Figure, AxesCommon, Caption, Color, Fix};
 
+
 fn main() {
 
     // Physical constants (SI units and energy in eV unless noted)
@@ -123,4 +124,28 @@ fn prompt(message: &str, default: f64) -> f64 {
     //     Ok(num) => num,
     //     Error(_) => default
     // }
+}
+
+fn plot(energies: &Vec<f64>, stopping_powers: &Vec<f64>, 
+        caption: &str, title: &str){
+    // --- Plotting using gnuplot ---
+    let mut fg = Figure::new();
+    {
+        let axes = fg.axes2d();
+        // Set logarithmic scales for both axes
+        axes.set_x_log(Some(2.0));
+        axes.set_y_log(Some(2.0));
+        // Set axis ranges
+        axes.set_x_range(Fix(10.0), Fix(10500.0));
+        axes.set_y_range(Fix(1e-30),Fix(1e-27));
+        // Set titles and labels
+        axes.set_title(title, &[]);
+        axes.set_x_label("Energía (MeV)", &[]);
+        axes.set_y_label("Poder de frenado (MeV/cm)", &[]);
+        // Plot the data in blue with a label
+        axes.lines(energies, stopping_powers, &[Caption(caption), Color("blue")]);
+    }
+    // Set terminal to PNG (size 1000x600) and display the plot
+    fg.set_terminal("pngcairo size 1000,600", "plot.png");
+    fg.show().expect("Unable to show plot");
 }
